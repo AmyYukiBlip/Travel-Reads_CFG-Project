0f24f7b75e6473f86c35c8a73f615cd869acd604
Amy <amy_hanwell@hotmail.com>
2025-02-03 17:07:25 +0000

    Refactors scripts into one file, adds css's to folder

---
+++ readme.md
@@ -0,0 +1,8 @@
+# TravelReads 
+
+Start here 🖝 https://travel-reads-cfg-project.onrender.com 🖜 to view the hosted project!
+
+## Project Summary
+
+TravelReads is a 3 page responsive website built with HTML, CSS and Javascript and some Bootstrap functionality.
+
--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function displayGreeting() {
     alert("No email entered, try again 😊");
   }
 }
+
+
+function bookSuggestion(){
+    let bookName = document.getElementById('bookInput').value;
+    if (bookName == null || bookName == "") {
+        alert('Please enter the title of a book.')
+    } else {
+        alert("Thanks for suggesting " + bookName + ", we'll add it to the reading list!");
+    }
+}
