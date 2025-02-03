e1aaf20f609c281a22b32618ec39385238293700
Amy <amy_hanwell@hotmail.com>
2025-02-03 17:29:22 +0000

    Removes duplicate pages

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function bookSuggestion(){
-    let bookName = document.getElementById('bookInput').value;
-    if (bookName == null || bookName == "") {
-        alert('Please enter the title of a book.')
-    } else {
-        alert("Thanks for suggesting " + bookName + ", we'll add it to the reading list!");
-    }
-}
